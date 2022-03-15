7252d2e0fe9216def3e4120e0f3d9362713ad09b
Matheus Oliveira 💻 <matheusvp2@ucl.br>
2022-03-15 17:55:22 -0300

    Adicionando page de reconferencia

---
+++ pages/reconferencia.jsx
@@ -0,0 +1,20 @@
+import { Button } from "primereact/button";
+import { FileUpload } from "primereact/fileupload";
+import { ProgressBar } from "primereact/progressbar";
+import { Tag } from "primereact/tag";
+import React, { useRef, useState } from "react";
+import Layout from "../components/Layout";
+
+
+export default function Contabilidade() {
+
+
+
+    return (
+        <>
+            <Layout>
+
+            </Layout>
+        </>
+    );
+}
