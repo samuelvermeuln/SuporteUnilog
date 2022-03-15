2ba9e46cab9b55625523d636f776a8335aa46b96
Samuel Vermeuln <vermeuln@ucl.br>
2022-03-15 18:03:58 -0300

    ....

--- pages/reconferencia.jsx
+++ pages/reconferencia.jsx
@@ -4,16 +4,27 @@ import { ProgressBar } from "primereact/progressbar";
 import { Tag } from "primereact/tag";
 import React, { useRef, useState } from "react";
 import Layout from "../components/Layout";
+import { PickList } from 'primereact/picklist';
 
-
-export default function Contabilidade() {
+export default function Reconferencia() {
 
 
 
     return (
         <>
             <Layout>
-
+                <PickList
+                    source={origen}
+                    target={destino}
+                    itemTemplate={itemTemplate}
+                    sourceHeader="Permissões"
+                    targetHeader="Permissões"
+                    sourceStyle={{ height: '250px' }}
+                    targetStyle={{ height: '250px' }}
+                    onChange={onChange}
+                    showSourceControls={false}
+                    showTargetControls={false}
+                />
             </Layout>
         </>
     );
