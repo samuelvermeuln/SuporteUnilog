f66fb3d4879629c4245cc938fbcff4c1cb8bd907
Matheus Oliveira <teteuoliveira12@gmail.com>
2022-03-16 00:37:25 -0300

    Resolvendo BO de pasta

--- pages/api/conferencia.js
+++ pages/api/conferencia.js
@@ -19,23 +19,25 @@ const readFileCsv = (path) => {
 export default async (req, res) => {
 
     try {
+        const dadosPath = fs.readdirSync(uploadPath);
+        console.log(dadosPath);
+        res.status(200).json( { error } );
+        // const { numero_onda } = req.body
+        // if( !numero_onda ) res.status(404).json({ error: "Informe o numero da onda." })
 
-        const { numero_onda } = req.body
-        if( !numero_onda ) res.status(404).json({ error: "Informe o numero da onda." })
+        // const uploadPath = '/app/upload'
+        // const arquivos = fs.readdirSync(uploadPath);
 
-        const uploadPath = '/app/upload'
-        const arquivos = fs.readdirSync(uploadPath);
+        // const isExistsOnda = arquivos.includes( numero_onda + '.csv' )
+        // if(!isExistsOnda) res.status(404).json({ error: "Onda não encontrada. Informe a onda para o suporte, subir o arquivo de verificação da onda." })
 
-        const isExistsOnda = arquivos.includes( numero_onda + '.csv' )
-        if(!isExistsOnda) res.status(404).json({ error: "Onda não encontrada. Informe a onda para o suporte, subir o arquivo de verificação da onda." })
+        // const pathArquivoOnda = path.join( uploadPath, numero_onda + '.csv' )
+        // const dados = await readFileCsv(pathArquivoOnda)
 
-        const pathArquivoOnda = path.join( uploadPath, numero_onda + '.csv' )
-        const dados = await readFileCsv(pathArquivoOnda)
+        // // Remove a primeira posicao do array
+        // dados.shift()
 
-        // Remove a primeira posicao do array
-        dados.shift()
-
-        res.status(200).json( { pedidos: dados } );
+        // res.status(200).json( { pedidos: dados } );
     } catch (error) {
         res.status(500).json( { error } );
     }
