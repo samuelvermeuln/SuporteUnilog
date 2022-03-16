1725ad6a6b1bd9986d34917efe2ec8d0c031e88b
Matheus Oliveira <teteuoliveira12@gmail.com>
2022-03-16 00:10:16 -0300

    Add Listar e Deletar arquivos upload

---
+++ pages/api/controle.js
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+export default async (req, res) => {
+
+    const { action } = req.body
+    if(!action) res.status(404).json({ error: "Informe a ação." })
+
+    const validateActions = [ 'LISTAR', 'DELETAR' ]
+    const isValidAction = validateActions.includes(action)
+    if( !isValidAction ) res.status(404).json({error: "Ação informada invalida."})
+
+    const uploadPath = './upload'
+
+    if( action == 'LISTAR' ){
+        const arquivos = fs.readdirSync(uploadPath);
+        res.status(200).json({ arquivos })
+    }
+
+    if( action == 'DELETAR' ){
+        try {
+            const arquivosDeletar = fs.readdirSync(uploadPath).map( item => path.join( uploadPath, item ) );
+            for (const arquivo of arquivosDeletar) {
+                fs.unlinkSync(arquivo)
+            }
+            res.status(200).json({ message: "Deletados com sucesso." })
+        } catch (error) {
+            res.status(404).json({ error })
+        }
+    }
+
+}
